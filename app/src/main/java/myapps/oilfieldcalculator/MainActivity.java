b8472c4ef08baeace93698d01ef780c480aed3fa
Alex <amorales88@utexas.edu>
2015-04-16 13:49:12 -0500

    Created the calculate average method

--- app/src/main/java/myapps/oilfieldcalculator/MainActivity.java
+++ app/src/main/java/myapps/oilfieldcalculator/MainActivity.java
@@ -2,6 +2,8 @@ package myapps.oilfieldcalculator;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,15 +15,30 @@ import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 
+    EditText upperTemp = (EditText) findViewById(R.id.TempUpper);
+    EditText middleTemp = (EditText) findViewById(R.id.TempMiddle);
+    EditText lowerTemp = (EditText) findViewById(R.id.TempLower);
+    TextView AvgTempNum = (TextView) findViewById(R.id.AvgTempNum);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        EditText upperTemp = (EditText) findViewById(R.id.TempUpper);
-        upperTemp.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+
+        upperTemp.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                Toast.makeText(getApplicationContext(), "Fire!", Toast.LENGTH_SHORT).show();
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                AvgTempNum.setText(Double.toString(calculateAverageTemp()));
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
 
             }
         });
@@ -50,5 +67,15 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    public double calculateAverageTemp(){
+        double result = 0.0;
+        //Check to see if field is blank, if so it will cause an error
+        result =+ (upperTemp.getText().toString().matches("")? 0 : Double.valueOf(upperTemp.getText().toString()));
+        result =+ (middleTemp.getText().toString().matches("")? 0 : Double.valueOf(middleTemp.getText().toString()));
+        result =+ (lowerTemp.getText().toString().matches("")? 0 : Double.valueOf(lowerTemp.getText().toString()));
+        return (result/3);
     }
 }
